b79900d016293bcd248601317535f59c95010abe
Murillo Vieira e Silva <murillo.vieiraesilva@gmail.com>
2018-11-26 15:05:09 -0200

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  user: firebase.User;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private ngFireAuth: AngularFireAuth) {
+    ngFireAuth.authState.subscribe(loginInfo => {
+      this.user = loginInfo;
+    });
   }
 
+  login() {
+    this.ngFireAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+  }
 }
