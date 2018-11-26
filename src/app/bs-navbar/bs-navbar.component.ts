a42fb3c3b9a9ae404f8c2bc432e00a5e91b7c2c3
Murillo Vieira e Silva <murillo.vieiraesilva@gmail.com>
2018-11-26 15:04:21 -0200

    Update bs-navbar.component.ts

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.scss']
 })
-export class BsNavbarComponent implements OnInit {
+export class BsNavbarComponent {
+  user$: Observable<firebase.User>;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private ngFireAuth: AngularFireAuth) {
+    this.user$ = ngFireAuth.authState;
   }
 
+  logout() {
+    this.ngFireAuth.auth.signOut();
+  }
 }
